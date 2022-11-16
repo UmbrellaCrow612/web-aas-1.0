27a530bedb0db7d45537031be8562645fc2efd0d
yousaf wazir <umbrellaman612@gmail.com>
2022-11-16 15:19:20 +0000

    feat: added data fetching using prisma

--- src/pages/temp.tsx
+++ src/pages/temp.tsx
@@ -1,10 +1,18 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function Page() {
-  
+
+  useEffect(() => {
+    // data fetching here
+    fetch('/api/customers')
+      .then((response) => response.json())
+      .then((actualData) => console.log(actualData))
+      .catch((err) => {
+        alert(err.message)
+      })
+  }, [])
   return <></>
 }
 
-
-// To fetch the data make an endpoint to hit 
-// use fetch here 
+// To fetch the data make an endpoint to hit
+// use fetch here
