afd8b45fbabd4c3c5ab789d572b2e111fe327788
yousaf wazir <umbrellaman612@gmail.com>
2022-11-16 13:15:56 +0000

    feat: added create page

--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -37,21 +37,13 @@ export default function Page() {
           <p>Account</p>
           <select className="w-full max-w-xs select select-bordered">
             <option disabled selected>
-              Who shot first?
+              Account
             </option>
-            <option>Han Solo</option>
-            <option>Greedo</option>
+            <option>Simple deposit</option>
+            <option>Long term deposit</option>
+            <option>Current account</option>
           </select>
-          <p>Special customer</p>
-          <div className="flex items-center justify-center w-full gap-4 py-2">
-            <div>
-              <p>Yes</p> <input type="radio" name="radio-1" className="radio" />
-            </div>
-            <div>
-              <p>No</p> <input type="radio" name="radio-1" className="radio" />
-            </div>
-          </div>
-          <div className="justify-end card-actions">
+          <div className="justify-end py-5 card-actions">
             <button className="btn btn-primary">Create</button>
             <button className="btn btn-primary">Clear</button>
           </div>
