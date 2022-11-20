d6305bc47f4c92ac0cba3b7b771c2099015f198a
yousaf wazir <umbrellaman612@gmail.com>
2022-11-20 17:30:21 +0000

    ci: added api instructions

--- src/pages/api/customer.tsx
+++ src/pages/api/customer.tsx
@@ -1,1 +1,12 @@
-export {}
+export { }
+
+/*
+
+Get method = take id number return the customer details 
+
+Post - take body - destructor - add to db 
+
+Patch - get body - update the field in db
+
+
+*/
