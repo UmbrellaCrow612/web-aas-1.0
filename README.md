278e756684364643b0d5798d6fa0411a7663b656
yousaf wazir <umbrellaman612@gmail.com>
2022-11-20 17:07:38 +0000

    feat: added new schema

--- README.md
+++ README.md
@@ -13,3 +13,14 @@ Set up branch
 pscale branch create star-app initial-setup
 ```
 
+Run the local branch in the terminal 
+
+```
+pscale connect dbName dbBranchName --port 3309
+```
+
+Push schema changes here 
+
+```
+npx prisma db push
+```
--- src/pages/api/customers.ts
+++ src/pages/api/customers.ts
@@ -1,7 +1,7 @@
 import prisma from '../../lib/prisma'
 
 // Optional fields in body: content
-export default async function handle(req:any, res:any) {
-  const result = await prisma.customers.findMany()
-   res.status(200).json(result)
-}
+// export default async function handle(req:any, res:any) {
+//   const result = await prisma.customers.findMany()
+//    res.status(200).json(result)
+// }
