23cac83d01da967662bb5e30ec4f00bb2752af4b
yousaf wazir <umbrellaman612@gmail.com>
2022-11-20 17:41:14 +0000

    feat: added the api endpoint

--- src/pages/api/customer.tsx
+++ src/pages/api/customer.tsx
@@ -1,4 +1,28 @@
-export { }
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  let method = req.method
+
+  switch (method) {
+    case (method = 'GET'):
+      res.status(200).json([{ name: 'Hello get method' }])
+      break
+    case (method = 'POST'):
+      res.status(200).json([{ name: 'Hello post method' }])
+      break
+
+    case (method = 'PATCH'):
+      res.status(200).json([{ name: 'Hello patch method' }])
+      break
+
+    default:
+      res.status(400)
+      break
+  }
+}
 
 /*
 
