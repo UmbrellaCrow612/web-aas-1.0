d5cc4d9acb89f3997313dfb9822ac00cd07f2ec2
yousaf wazir <umbrellaman612@gmail.com>
2022-11-22 20:19:11 +0000

    feat: added search user

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,10 +1,21 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import { useState } from 'react'
 
 interface SearchBarProps {}
 
 export const SearchBar: React.FC<SearchBarProps> = () => {
+  const [searchTerm, setSearchTerm] = useState('')
   const router = useRouter()
+
+   const keyDownHandler = (event:any) => {
+
+     if (event.key === 'Enter') {
+       event.preventDefault()
+       
+       router.push(`/customers/${searchTerm}`)
+     }
+   }
   return (
     <div className="justify-between px-4 mx-auto navbar bg-base-100 max-w-screen-2xl">
       <Link href="/">
@@ -14,7 +25,11 @@ export const SearchBar: React.FC<SearchBarProps> = () => {
         type="text"
         placeholder="Search a user"
         className="w-full max-w-xs input input-bordered"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        onKeyDown={(e) => keyDownHandler(e)}
       />
+
       <button className="btn btn-outline" onClick={() => router.push('/login')}>
         Logout
       </button>
