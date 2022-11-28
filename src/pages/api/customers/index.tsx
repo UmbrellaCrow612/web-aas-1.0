4138246863badf599e3075afbd8905e9c44c6956
yousaf wazir <umbrellaman612@gmail.com>
2022-11-28 09:10:14 +0000

    fix: removed to many if statements

--- src/pages/api/customers/index.tsx
+++ src/pages/api/customers/index.tsx
@@ -10,84 +10,44 @@ export default async function handle(
     return res.status(200).json(customers)
   } else if (req.method == 'POST') {
     // Take stuff out
-    const { firstName, lastName, account, address, annualSalary, age } =
+    const { firstName, lastName, address, annualSalary, age } =
       req.body
-    if (account == 'Simple deposit') {
-      let result = await prisma.customer.create({
-        data: {
-          firstName: firstName,
-          lastName: lastName,
-          address: address,
-          simpleDeposits: {
-            create: {
-              balance: 0,
-            },
-          },
-          annualSalary: annualSalary,
-          age: age,
-          cards: {
-            create: {
-              cardNumber: 123456789,
-            },
+    let result = await prisma.customer.create({
+      data: {
+        firstName: firstName,
+        lastName: lastName,
+        address: address,
+        simpleDeposits: {
+          create: {
+            balance: 0,
           },
         },
-      })
-      res.json(result)
-    } else if (account == 'Long term deposit') {
-      let result = await prisma.customer.create({
-        data: {
-          firstName: firstName,
-          lastName: lastName,
-          address: address,
-          longtermDeposits: {
-            create: {
-              balance: 0,
-            },
-          },
-          annualSalary: annualSalary,
-          age: age,
-          cards: {
-            create: {
-              cardNumber: 123456789,
-            },
+        longtermDeposits: {
+          create: {
+            balance: 0,
           },
         },
-      })
-      res.json(result)
-    } else if (account == 'Withdraw deposits') {
-      let result = await prisma.customer.create({
-        data: {
-          firstName: firstName,
-          lastName: lastName,
-          address: address,
-          withdrawDeposits: {
-            create: {
-              balance: 0,
-            },
-          },
-          annualSalary: annualSalary,
-          age: age,
-          cards: {
-            create: {
-              cardNumber: 123456789,
-            },
+        withdrawDeposits: {
+          create: {
+            balance: 0,
           },
         },
-      })
-      res.json(result)
-    }
-
-    // do logic here for wihc accoutn type then do the inter result again but with the specifc account created
+        annualSalary: annualSalary,
+        age: age,
+        cards: {
+          create: {
+            cardNumber: Number(Math.random().toString().slice(2,11)),
+          },
+        },
+      },
+    })
+    res.json(result)
   }
 }
 
 /*
 
-Get method = take id number return the customer details 
 
-Post - take body - destructor - add to db 
-
-Patch - get body - update the field in db
 
 
 */
